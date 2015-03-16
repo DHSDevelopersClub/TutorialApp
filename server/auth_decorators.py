d815e2c1736e130fd0ce1213ea7b9e6162e3208c
sebastian <lord.of.all.sebastian@gmail.com>
2015-03-15 19:54:44 -0700

    Add auto student regristration

--- server/auth_decorators.py
+++ server/auth_decorators.py
@@ -20,3 +20,21 @@ def requires_student(func, instance, args, kwargs):
         raise endpoints.UnauthorizedException('Invalid token.')
     kwargs['current_user'] = current_user
     return func(*args, **kwargs)
+
+@wrapt.decorator
+def requires_teacher(func, instance, args, kwargs):
+    current_user = endpoints.get_current_user()
+    teacher_list = [] # TODO for Sebastian: make datastore object
+    if current_user not in teacher_list:
+        raise endpoints.UnauthorizedException('Invalid token')
+    kwargs['current_user'] = current_user
+    return func(*args, **kwargs)
+
+@wrapt.decorator
+def requires_admin(func, instance, args, kwargs):
+    current_user = endpoints.get_current_user()
+    teacher_list = [] # TODO for Sebastian: make datastore object
+    if current_user not in teacher_list:
+        raise endpoints.UnauthorizedException('Invalid token')
+    kwargs['current_user'] = current_user
+    return func(*args, **kwargs)
