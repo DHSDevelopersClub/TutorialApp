f2d4d9987b25ab873fa523c0c9e8eeac28b35611
Alexander Otavka <zotavka@gmail.com>
2015-03-25 13:06:05 -0700

    Tweak client structure.

---
+++ web_client/static/app.js
@@ -0,0 +1,174 @@
+/**
+ * app.js
+ *
+ * Javascript (for app auto-binding template) shared across clients.
+ *
+ * Author: Alexander Otavka
+ */
+
+var app = document.querySelector("template#app");
+app.search = "";
+app.date = "";
+
+app.addEventListener("template-bound", function() {
+    "use strict";
+
+    var actionBarSearchAnimation = (function() {
+        /* Search transition animation for the action bar. */
+        var green = "#4CB04F";
+        var grey = "#ABABAB";
+
+        var anim = new CoreAnimationGroup();
+        anim.type = "par";
+        anim.duration = 200;
+
+        var bg = new CoreAnimation();
+        bg.target = app.$.actionBarToolbar;
+        bg.fill = "forwards";
+        bg.keyframes = [{backgroundColor: green},
+                        {backgroundColor: grey}];
+        anim.appendChild(bg);
+
+        var buttons = new CoreAnimation();
+        buttons.target = app.$.actionButtons;
+        buttons.fill = "forwards";
+        buttons.keyframes = [{opacity: 1},
+                             {opacity: 0}];
+        anim.appendChild(buttons);
+
+        var nav = new CoreAnimation();
+        nav.target = app.$.navWrapper;
+        nav.fill = "forwards";
+        var default_nav_keyframes = [{opacity: 0, left: "-15px"},
+                                     {opacity: 1, left: "0px"}];
+        var reverse_nav_keyframes = [{opacity: 0, left: "15px"},
+                                     {opacity: 1, left: "0px"}];
+        nav.keyframes = default_nav_keyframes;
+        anim.appendChild(nav);
+
+        anim.playReverse = function () {
+            bg.direction = "reverse";
+            buttons.direction = "reverse";
+            nav.keyframes = reverse_nav_keyframes;
+            anim.play();
+            bg.direction = "normal";
+            buttons.direction = "normal";
+            nav.keyframes = default_nav_keyframes;
+        };
+
+        return anim;
+    })();
+    var searchBoxResizeAnimation = (function() {
+        var anim = new CoreAnimationGroup();
+        anim.type = "par";
+        anim.duration = 200;
+
+        var nav = new CoreAnimation();
+        nav.target = app.$.navWrapper;
+        anim.appendChild(nav);
+
+        var ab = new CoreAnimation();
+        ab.target = app.$.actionButtons;
+        anim.appendChild(ab);
+
+        anim.setKeyframes = function (newKeyframes) {
+            for (var i=0; i < anim.children.length; i++) {
+                anim.children[i].keyframes = newKeyframes;
+            }
+        };
+
+        return anim;
+    })();
+
+    app.$.apiLoader.setOnload(function() {
+        gapi.client.dhstutorial.next_tutorial().execute(function(response) {
+            app.date = response.date;
+        });
+    });
+
+    app.onMediaMobileChange = function() {
+        /* Align the search bar based on media and search state. */
+
+        var inSearchMode = function () {
+            return (app.$.searchBar.mode === app.$.searchBar.MODE_SEARCH);
+        };
+
+        if (app.mediaIsMobile && inSearchMode()) {
+            app.$.actionButtons.setAttribute("hidden", "");
+            app.$.navBackText.setAttribute("hidden", "");
+            app.$.searchBar.$.boxWrapper.style.textAlign = "left";
+        } else {
+            app.$.actionButtons.removeAttribute("hidden");
+            app.$.navBackText.removeAttribute("hidden");
+            app.$.searchBar.$.boxWrapper.style.textAlign = "center";
+        }
+
+        if (!inSearchMode()) {
+            for (var i = 0; i < app.$.actionButtons.children.length; i++) {
+                app.$.actionButtons.children[i].removeAttribute("hidden");
+            }
+        }
+
+        var oldNavWid;
+        var oldABWid;
+        if (!app.mediaIsMobile) {
+            oldNavWid = app.$.navWrapper.offsetWidth;
+            oldABWid = app.$.actionButtons.offsetWidth;
+        }
+
+        app.$.navWrapper.style.width = "auto";
+        app.$.actionButtons.style.width = "auto";
+
+        if (!app.mediaIsMobile) {
+            var widthMargin = Math.max(app.$.actionButtons.offsetWidth,
+                                       app.$.navWrapper.offsetWidth) + 2;
+            var snapToWidth = function () {
+                app.$.navWrapper.style.width = widthMargin + "px";
+                app.$.actionButtons.style.width = widthMargin + "px";
+
+                if (app.$.searchBar.mode === app.$.searchBar.MODE_SEARCH) {
+                    for (var i = 0; i < app.$.actionButtons.children.length; i++) {
+                        app.$.actionButtons.children[i].setAttribute("hidden", "");
+                    }
+                }
+            };
+            if (oldABWid === oldNavWid) {
+                var oldWidth = oldNavWid;
+                searchBoxResizeAnimation.setKeyframes(
+                        [{width: oldWidth + "px"},
+                         {width: widthMargin + "px"}]);
+                // TODO: make fill = "forwards" work, so slower browsers look good
+                searchBoxResizeAnimation.addEventListener(
+                        "core-animation-finish", snapToWidth);
+                searchBoxResizeAnimation.play();
+            } else {
+                snapToWidth();
+            }
+        }
+    };
+    app.onMediaMobileChange();
+
+    app.enableSearchMode = function() {
+        actionBarSearchAnimation.play();
+
+        app.$.navTitle.setAttribute("hidden", "");
+        app.$.navBack.removeAttribute("hidden");
+        app.onMediaMobileChange();
+    };
+
+    app.disableSearchMode = function() {
+        if (app.$.searchBar.mode !== app.$.searchBar.MODE_SEARCH) return;
+
+        app.$.searchBar.disableSearchMode();
+        actionBarSearchAnimation.playReverse();
+
+        app.$.navBack.setAttribute("hidden", "");
+        app.$.navTitle.removeAttribute("hidden");
+        app.onMediaMobileChange();
+    };
+
+    app.signOut = function() {
+        gapi.auth.setToken(null);
+        app.refresh(true);
+    };
+});
--- web_client/static/teacher/app.js
+++ web_client/static/teacher/app.js
@@ -5,3 +5,13 @@
  *
  * Author: Alexander Otavka
  */
+
+var app = document.querySelector("template#app");
+
+app.addEventListener("template-bound", function() {
+    "use strict";
+
+    app.refresh = function() {
+        console.log("debug refresh");
+    };
+});
