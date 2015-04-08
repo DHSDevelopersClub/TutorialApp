1a7eef96f04dfc6bc2c174f16f5c456d3541601d
sebastian <lord.of.all.sebastian@gmail.com>
2015-04-07 19:19:16 -0700

    Work on settings

--- web_client/static/settings.js
+++ web_client/static/settings.js
@@ -1,3 +1,11 @@
+/**
+ * settings.js
+ *
+ * Javascript (for app auto-binding template) shared across clients.
+ *
+ * Author: Sebastian Boyd
+ */
+
 var settings = document.querySelector("#settings");
 
 settings.addEventListener("template-bound", function() {
